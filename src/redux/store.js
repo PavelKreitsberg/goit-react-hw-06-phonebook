bf4240861c4d0f149daf5ac457408964187856e2
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-11-20 20:31:41 +0200

    add react-persist(adding to local storage)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { filterReducer } from './filter/filterSlice';
 import { contactsReducer } from "./contacts/contactsSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const rootReducer = combineReducers({
+    filter: filterReducer,
+    contacts: contactsReducer,
+})
 
+const persistConfig = {
+  key: 'root',
+    storage,
+  whitelist: ['contacts']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filter: filterReducer,
-   },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+
 })
+
+export const persistor = persistStore(store)
